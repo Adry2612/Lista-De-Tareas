ed9783136edcf32d5e207f0ae4627993d35ce0ed
Adry2612 <adrianvidalopez12@gmail.com>
2020-05-05 13:21:00 +0200

    Integracion de texto y botones

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 'use strict';
 
+// El script se ejecutará una vez se haya cargado todo el documento HTML.
 document.addEventListener('DOMContentLoaded', function()
 {
-    
+    // Selectores
+    const boton = document.querySelector('form button');
+    const contenedor = document.querySelector('.listaTareas');
+
     // Al pulsar el botón de añadir se captura el texto del formulario y se creará un nuevo div con el contenido de este.
-    var boton = document.querySelector('form button');
-    document.preventDefault(boton);
     boton.addEventListener('click', addTarea);
     
     function addTarea(event)
     {
-        
-        
-        var contenedor = document.querySelector('.listaTareas');
-        var texto = document.querySelector('#tarea').value;
-        
+        // Evitamos que la página se refresque al pulsar el botón.
+        event.preventDefault();
+
+        // Creamos los elementos necesarios para realizar la estructura HTML.
         var newDiv = document.createElement('div');
-        newDiv.appendChild(texto);
+        newDiv.classList.add('tarea');
+        
+        // Elemento de lista.
+        var li = document.createElement('li');
+        li.classList.add('nuevaTarea');
+
+        // Botones que se encontrarán dentro de cada uno de los contenedor de tareas.
+        var finalizada = document.createElement('button');
+        finalizada.innerHTML ='<i class="fas fa-check"></i>';
+        finalizada.classList.add('completada');
+        
+        var borrar = document.createElement('button');
+        borrar.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        borrar.classList.add('eliminar');
+
+        // Obtenemos el texto del campo para agregarlo al contenedor.
+        var texto = document.getElementById('tarea').value;
+        console.log(texto);
+        // Añadir todos los elementos al contenedor de tareas.
+        li.innerHTML = texto;
+        newDiv.appendChild(li);
+        newDiv.appendChild(finalizada);
+        newDiv.appendChild(borrar);
         contenedor.appendChild(newDiv);
-
-        alert('hoal');
-
+        
+        
     }
 });
