38e87a3568a74d5f9aa726e14a82f91ba0e365cb
Adry2612 <adrianvidalopez12@gmail.com>
2020-05-08 12:53:43 +0200

    Funcionalidad botones y animaciones al completaro eliminar tareas

--- app.js
+++ app.js
@@ -4,44 +4,95 @@
 document.addEventListener('DOMContentLoaded', function()
 {
     // Selectores
-    const boton = document.querySelector('form button');
-    const contenedor = document.querySelector('.listaTareas');
+    var agregar = document.querySelector('form button');
 
     // Al pulsar el botón de añadir se captura el texto del formulario y se creará un nuevo div con el contenido de este.
-    boton.addEventListener('click', addTarea);
-    
-    function addTarea(event)
-    {
-        // Evitamos que la página se refresque al pulsar el botón.
-        event.preventDefault();
-
-        // Creamos los elementos necesarios para realizar la estructura HTML.
-        var newDiv = document.createElement('div');
-        newDiv.classList.add('tarea');
-        
-        // Elemento de lista.
-        var li = document.createElement('li');
-        li.classList.add('nuevaTarea');
-
-        // Botones que se encontrarán dentro de cada uno de los contenedor de tareas.
-        var finalizada = document.createElement('button');
-        finalizada.innerHTML ='<i class="fas fa-check"></i>';
-        finalizada.classList.add('completada');
-        
-        var borrar = document.createElement('button');
-        borrar.innerHTML = '<i class="fas fa-trash-alt"></i>';
-        borrar.classList.add('eliminar');
-
-        // Obtenemos el texto del campo para agregarlo al contenedor.
-        var texto = document.getElementById('tarea').value;
-        console.log(texto);
-        // Añadir todos los elementos al contenedor de tareas.
-        li.innerHTML = texto;
-        newDiv.appendChild(li);
-        newDiv.appendChild(finalizada);
-        newDiv.appendChild(borrar);
-        contenedor.appendChild(newDiv);
-        
-        
-    }
+    agregar.addEventListener('click', addTarea);
+   
 });
+
+// Funciones
+// Añadir la tarea.
+function addTarea(event)
+{
+    // Evitamos que la página se refresque al pulsar el botón.
+    event.preventDefault();
+    
+    // Creamos los elementos necesarios para realizar la estructura HTML.
+    var newDiv = document.createElement('div');
+    newDiv.classList.add('tarea');
+    
+    // Elemento de lista.
+    var li = document.createElement('li');
+    li.classList.add('nuevaTarea');
+    
+    // Botones que se encontrarán dentro de cada uno de los contenedor de tareas. Añadimos un evento click para que realicen la función especificada además de una clase identificativa.
+    var finalizada = document.createElement('button');
+    finalizada.innerHTML ='<i class="fas fa-check"></i>';
+    finalizada.classList.add('completada');
+    finalizada.addEventListener('click', completarTarea);
+    
+    var borrar = document.createElement('button');
+    borrar.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    borrar.classList.add('eliminar');
+    borrar.addEventListener('click', eliminarTarea);
+    
+    // Obtenemos el selector del contenedor principal donde guardaremos las tareas.
+    var contenedor = document.querySelector('.listaTareas');
+
+    // Obtenemos el texto del campo para agregarlo al contenedor.
+    var texto = document.getElementById('tarea');
+    
+    // Añadir todos los elementos al contenedor de tareas.
+    li.innerHTML = texto.value;
+    newDiv.appendChild(li);
+    newDiv.appendChild(finalizada);
+    newDiv.appendChild(borrar);
+    contenedor.appendChild(newDiv);
+
+    // Eliminamos el texto que se encuentra en el campo Input.
+    texto.value = "";
+}
+
+// Marcar la tarea como completada.
+function completarTarea(event)
+{
+    // Eliminamos el comportamiento por defecto al pulsar el botón.
+    event.preventDefault();
+
+    // Agregamos la clase tareaCompletada al elemento padre.
+    this.parentElement.classList.add('tareaCompletada');
+}
+
+// Eliminar la tarea de la lista.
+function eliminarTarea(event)
+{
+    // Eliminamos el comportamiento por defecto al pulsar el botón.
+    event.preventDefault();
+
+    var elemento = this.parentElement;
+    // Agregamos la clase tareaEliminada para que realice la animación indicada mediante CSS.
+    this.parentElement.classList.add('tareaEliminada');
+
+    // Una vez finalizada la animación, obtenemos el elemento padre del botón seleccionado (ya que es el que contendrá la tarea en su totalidad), y lo eliminamos del DOM.
+    this.parentElement.addEventListener('transitionend', function()
+    {
+        this.remove();
+    });
+}
+
+// Comprobamos si el campo Input esta vacio, si lo esta al pulsar el botón no se deberá añadir ninguna tarea y se deberá mostrar un mensaje de error.
+function comprobarVacio()
+{
+    var input = document.querySelector('.tarea');
+
+    if (input.value == "")
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
